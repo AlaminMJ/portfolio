9ca9396a10c5922679720854572a8c3aa975de89
Alamin <alamin.mj.3228@gmail.com>
2021-11-30 20:31:16 +0600

    code push

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("connect");
+const more = document.querySelectorAll(".more");
+const close = document.querySelectorAll(".close");
+
+more.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.classList.add("active");
+  });
+});
+
+close.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    console.log(e.target.parentElement.parentElement.parentElement);
+    e.target.parentElement.parentElement.classList.remove("active");
+  });
+});
